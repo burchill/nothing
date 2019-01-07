35471243e10176bf6bd34eaa5d38e69c66a7cf88
Zach Burchill <burchill@users.noreply.github.com>
2019-01-07 12:26:02 -0500

    doesn't unload other base packages now

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
     suppressMessages(
         if (Sys.getenv("R_INSTALL_PKG") != "nothing") {
             repeat{
-                pkgs <- setdiff(loadedNamespaces(), "base")
+                pkgs <- setdiff(loadedNamespaces(), sessionInfo()$basePkgs)
                 if (!length(pkgs)) break
                 for (pkg in pkgs) {
                     try(unloadNamespace(pkg), silent = TRUE)
